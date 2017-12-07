b4447a9ad3c5e32184c50913eb1a9cea29fe4295
lokth <thapa.lok19@gmail.com>
2017-12-07 18:13:54 -0500

    todo service implementation

--- src/main/java/com/thisistime/springmockito/SpringMockitoPractice/ToDoServiceImpl.java
+++ src/main/java/com/thisistime/springmockito/SpringMockitoPractice/ToDoServiceImpl.java
@@ -1,4 +1,32 @@
 package com.thisistime.springmockito.SpringMockitoPractice;
 
-public class ToDoServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
+public class ToDoServiceImpl  implements ToDoServices{
+
+    @Autowired
+    private ToDoRepository toDoRepository;
+
+    @Override
+    public List<ToDo> getAllToDo() {
+        return toDoRepository.findAll();
+    }
+
+    @Override
+    public ToDo getToDoById(long id) {
+        return toDoRepository.findOne(id);
+    }
+
+    @Override
+    public ToDo saveToDo(ToDo toDo) {
+        return toDoRepository.save(toDo);
+    }
+
+    @Override
+    public void removeTodo(ToDo toDo) {
+        toDoRepository.delete(toDo);
+    }
 }
