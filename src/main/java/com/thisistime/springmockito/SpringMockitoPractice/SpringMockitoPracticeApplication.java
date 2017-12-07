ec87906e0f722df71d6de3d3befd2e4779d520af
lokth <thapa.lok19@gmail.com>
2017-12-07 10:28:36 -0500

    initial commit

---
+++ src/main/java/com/thisistime/springmockito/SpringMockitoPractice/SpringMockitoPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.thisistime.springmockito.SpringMockitoPractice;
+
+		import org.springframework.boot.SpringApplication;
+		import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockitoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMockitoPracticeApplication.class, args);
+	}
+}
